e8585fda18736b422fa0d555796a0afce5649a7c
syoich <packisyi@gmail.com>
2020-01-01 01:58:25 +0000

    PHP/Laravel 14課題

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+// 以下を追記することでNews Modelが扱えるようになる
+use App\News;
 
 class NewsController extends Controller
 {
@@ -14,6 +16,30 @@ class NewsController extends Controller
     }
     public function create(Request $request)
   {
+      // 以下を追記
+      // Validationを行う
+      $this->validate($request, News::$rules);
+
+      $news = new News;
+      $form = $request->all();
+
+      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+
+      // データベースに保存する
+      $news->fill($form);
+      $news->save();
+      
       // admin/news/createにリダイレクトする
       return redirect('admin/news/create');
   }  
--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+// 以下を追記することでProfile Modelが扱えるようになる
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -13,10 +15,26 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create(Request $request)
-    {
-        return redirect('admin/profile/create');
-    }
+public function create(Request $request)
+  {
+
+      // 以下を追記
+      // Validationを行う
+      $this->validate($request, profile::$rules);
+      $profile = new Profile;
+      $form = $request->all();
+
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+
+      return redirect('admin/profile/create');
+  }
 
     public function edit()
     {
