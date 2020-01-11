773b48d44898a099446afe947e64150f606b83a6
syoich <packisyi@gmail.com>
2020-01-11 05:31:25 +0000

    PHP/Laravel 18課題

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -23,8 +23,8 @@
         <!-- Styles -->
         {{-- Laravel標準で用意されているCSSを読み込みます --}}
         <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-        {{-- この章の後半で作成するCSSを読み込みます --}}
-        <link href="{{ secure_asset('css/profile.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/front.css') }}" rel="stylesheet">
+        
     </head>
     <body>
         <div id="app">
